709989c0f6a79d4dc67bb4ff1ca5734b63b16746
sankirth123 <49102408+sankirth123@users.noreply.github.com>
2024-12-29 14:10:30 -0500

    Weather_Observation_10.sql

---
+++ SQL/Weather_Observation_10.sql
@@ -0,0 +1,25 @@
+/*
+
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE CITY REGEXP '[^aeiouAEIOU]$'
+ORDER BY CITY;
